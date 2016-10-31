e7d7c1873a37737e836e0cd77e20a4fda94043fe
wrx1721267632 <1721267632@qq.com>
2016-10-31 22:01:01 +0800

    update

--- init/entry.c
+++ init/entry.c
@@ -5,15 +5,20 @@
     > Created Time: 2016年10月23日 星期日 22时53分17秒
  ************************************************************************/
 
-#include"../include/console.h"
+#include "../include/console.h"
+#include "../include/debug.h"
 
 int kern_entry()
 {
+    init_debug();
+
     //清屏
     console_clear();
 
     //输出字符串
     console_write_color("hello, kernel\n", rc_black, rc_green);
+
+    panic("test");
 
     return 0;
 }
