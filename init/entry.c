477adf7d4cbda3936a6c959741dfe5f69311a542
wrx1721267632 <1721267632@qq.com>
2016-11-02 21:05:51 +0800

    update

--- init/entry.c
+++ init/entry.c
@@ -5,20 +5,22 @@
     > Created Time: 2016年10月23日 星期日 22时53分17秒
  ************************************************************************/
 
-#include "../include/console.h"
-#include "../include/debug.h"
+#include "debug.h"
+#include "gdt.h"
+#include "console.h"
 
 int kern_entry()
 {
     init_debug();
+    init_gdt();
 
     //清屏
     console_clear();
 
+    //
+    print_cur_status();
+
     //输出字符串
     console_write_color("hello, kernel\n", rc_black, rc_green);
-
-    panic("test");
-
     return 0;
 }
