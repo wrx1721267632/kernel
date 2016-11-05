a09d929423c9adbaa61bf194328ee09b45c2d3eb
wrx1721267632 <1721267632@qq.com>
2016-11-05 16:49:45 +0800

    update

--- init/entry.c
+++ init/entry.c
@@ -8,11 +8,13 @@
 #include "debug.h"
 #include "gdt.h"
 #include "console.h"
+#include "idt.h"
 
 int kern_entry()
 {
     init_debug();
     init_gdt();
+    init_idt();
 
     //清屏
     console_clear();
@@ -22,5 +24,8 @@ int kern_entry()
 
     //输出字符串
     console_write_color("hello, kernel\n", rc_black, rc_green);
+
+    asm volatile ("int $0x3");
+    asm volatile ("int $0x4");
     return 0;
 }
