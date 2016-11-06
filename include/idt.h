5d364a7ca8e941903c324e3c5fb36276e3551dff
wrx1721267632 <1721267632@qq.com>
2016-11-06 21:55:16 +0800

    uodate

--- include/idt.h
+++ include/idt.h
@@ -99,4 +99,44 @@ void isr31();
 
 // 255用户自定义异常
 void isr255();
+
+// IRQ 处理函数
+void irq_handler(pt_regs *regs);
+
+// 定义IRQ
+#define IRQ0    32  // 电脑系统计时器
+#define IRQ1    33  // 键盘
+#define IRQ2    34  // 与 IRQ9 相连, MPU-401 MD使用
+#define IRQ3    35  // 串口设备
+#define IRQ4    36  // 串口设备
+#define IRQ5    37  // 建议声卡驱动
+#define IRQ6    38  // 软驱传输控制使用
+#define IRQ7    39  // 打印机传输控制使用
+#define IRQ8    40  // 即时时钟
+#define IRQ9    41  // 与 IRQ2 相接, 可设定给其他硬件
+#define IRQ10   42  // 建议网卡使用
+#define IRQ11   43  // 建议 AGP 显卡使用
+#define IRQ12   44  // 接 PS/2 鼠标, 也可设定给其他硬件
+#define IRQ13   45  // 协处理器使用
+#define IRQ14   46  // IDE0 传输控制使用
+#define IRQ15   47  // IDE1 传输控制使用
+
+// 声明 IRQ 函数
+// IRQ 中断请求:(Interrupt Request)
+void irq0();     32  // 电脑系统计时器
+void irq1();     33  // 键盘
+void irq2();     34  // 与 IRQ9 相连, MPU-401 MD使用
+void irq3();     35  // 串口设备
+void irq4();     36  // 串口设备
+void irq5();     37  // 建议声卡驱动
+void irq6();     38  // 软驱传输控制使用
+void irq7();     39  // 打印机传输控制使用
+void irq8();     40  // 即时时钟
+void irq9();     41  // 与 IRQ2 相接, 可设定给其他硬件
+void irq10();    42  // 建议网卡使用
+void irq11();    43  // 建议 AGP 显卡使用
+void irq12();    44  // 接 PS/2 鼠标, 也可设定给其他硬件
+void irq13();    45  // 协处理器使用
+void irq14();    46  // IDE0 传输控制使用
+void irq15();    47  // IDE1 传输控制使用
 #endif
