e982b823aa8e8b915643a6db1eae9cd7d149eed3
wrx1721267632 <1721267632@qq.com>
2016-11-08 22:09:42 +0800

    update

--- init/entry.c
+++ init/entry.c
@@ -10,6 +10,7 @@
 #include "console.h"
 #include "idt.h"
 #include "timer.h"
+#include "pmm.h"
 
 int kern_entry()
 {
@@ -29,6 +30,28 @@ int kern_entry()
     init_timer(200);
 
     //开启中断
-    asm volatile ("sti");
+    //asm volatile ("sti");
+
+    printk("kernel in memory start: 0x%x\n", kern_start);
+    printk("kernel in memory end: 0x%x\n", kern_end);
+    printk("kernel in memory used: %d KB\n\n", (kern_end - kern_start +
+                1023) / 1024);
+
+    show_memory_map();
+
+    init_pmm();
+
+    printk("\nThe count of Physical Memory Page is: %u\n\n", phy_page_count);
+    uint32_t allc_addr = NULL;
+    printk("Test Physical Memory Alloc: \n");
+    allc_addr = pmm_alloc_page();
+    printk("Alloc Physical addr: 0x%x\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk("Alloc Physical addr: 0x%x\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk("Alloc Physical addr: 0x%x\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk("Alloc Physical addr: 0x%x\n", allc_addr);
+
     return 0;
 }
