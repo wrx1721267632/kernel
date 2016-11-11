24faa1b4378a585869c4f705204c456f1defc91b
wrx1721267632 <1721267632@qq.com>
2016-11-11 20:44:42 +0800

    update

--- init/entry.c
+++ init/entry.c
@@ -11,8 +11,67 @@
 #include "idt.h"
 #include "timer.h"
 #include "pmm.h"
+#include "vmm.h"
 
-int kern_entry()
+// 内核初始化函数
+void kern_init();
+
+// 开启分页机制后的 Multiboot 数据指针
+multiboot_t *glb_mboot_ptr;
+
+// 开启分页机制后的内核栈
+char kern_stack[STACK_SIZE];
+
+// 内核使用的临时页表和页目录
+// 该地址必须是页对齐的地址,内存 0-640KB 肯定是空闲的
+// 将作用的函数或数据放入指定的段
+__attribute__((section(".init.data"))) pgd_t *pgd_tmp  = (pgd_t *)0x1000;
+__attribute__((section(".init.data"))) pgd_t *pte_low  = (pgd_t *)0x2000;
+__attribute__((section(".init.data"))) pgd_t *pte_high = (pgd_t *)0x3000;
+
+// 内核入口函数
+__attribute__((section(".init.text"))) void kern_entry()
+{
+    pgd_tmp[0] = (uint32_t)pte_low | PAGE_PRESENT | PAGE_WRITE;
+    pgd_tmp[PGD_INDEX(PAGE_OFFSET)] = (uint32_t)pte_high | PAGE_PRESENT |
+        PAGE_WRITE;
+
+    // 映射内核虚拟地址 4MB 到物理地址的前 4MB
+    int i;
+    for (i = 0; i < 1024; i++)
+    {
+        pte_low[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
+    }
+
+    // 映射 0x00000000-0x00400000 的物理地址到虚拟地址 0xC0000000-0xC04000000
+    for (i = 0; i < 1024; i++)
+    {
+        pte_high[i] = (i << 12) | PAGE_PRESENT | PAGE_WRITE;
+    }
+
+    // 设置临时页表
+    asm volatile("mov %0, %%cr3" : : "r" (pgd_tmp));
+
+    uint32_t cr0;
+
+    // 启用分页, 将 cr0 寄存器的分页位置为 1 就好
+    asm volatile ("mov %%cr0, %0" : "=r" (cr0));
+    cr0 |= 0x80000000;
+    asm volatile ("mov %0, %%cr0" : : "r"(cr0));
+
+    // 切换内核栈
+    uint32_t kern_stack_top = ((uint32_t)kern_stack + STACK_SIZE) & 0xFFFFFFF0;
+    asm volatile("mov %0, %%esp\n\t"
+                 "xor %%ebp, %%ebp" : : "r" (kern_stack_top));
+
+    // 更新全局 multiboot_t 指针
+    glb_mboot_ptr = mboot_ptr_tmp + PAGE_OFFSET;
+
+    // 调用内核初始化函数
+    kern_init();
+}
+
+void kern_init()
 {
     init_debug();
     init_gdt();
@@ -53,5 +112,7 @@ int kern_entry()
     allc_addr = pmm_alloc_page();
     printk("Alloc Physical addr: 0x%x\n", allc_addr);
 
-    return 0;
+    while (1) {
+        asm volatile("hlt");
+    }
 }
